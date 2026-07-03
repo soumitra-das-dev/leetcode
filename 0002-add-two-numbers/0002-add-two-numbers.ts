fdaad32e027614aee23b2fca36adef835a0a3c4f
soumitra-das-dev <dsoumitra693@gmail.com>
2026-07-03 22:55:34 +0530

    Time: 3 ms (69.62%), Space: 62.4 MB (34.37%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.ts
+++ 0002-add-two-numbers/0002-add-two-numbers.ts
@@ -12,23 +12,19 @@
 
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
     let carry = 0;
-    let node1 = l1;
-    let node2 = l2;
-    let bufferNode = new ListNode(0, null);
-    let listNode = bufferNode;
-    while (node1 || node2) {
-        let acc = ((node1?.val ?? 0) + (node2?.val ?? 0) + carry)
+    let currNode = new ListNode(0, null);
+    let listNode = currNode;
+    while (l1 || l2 || carry) {
+        let acc = ((l1?.val ?? 0) + (l2?.val ?? 0) + carry)
         let sum = acc % 10;
         carry = ~~(acc / 10);
-        bufferNode.next = new ListNode(sum, null);
-        node1 = node1?.next ?? null;
-        node2 = node2?.next ?? null;
-        bufferNode = bufferNode.next
+        currNode.next = new ListNode(sum);
+        currNode = currNode.next
+
+        l1 = l1?.next ?? null;
+        l2 = l2?.next ?? null;
     }
 
-    if (carry != 0) {
-        bufferNode.next = new ListNode(carry, null)
-    }
 
     return listNode.next
 };
